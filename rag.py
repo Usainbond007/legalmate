b02eb104038caecf8b27ab7266d7a95c1f6802e5
Ishan Chowdhury <10.ishanc@gmail.com>
2026-03-27 04:36:58 +0530

    final working version with streamlit + gemini fix

--- rag.py
+++ rag.py
@@ -3,10 +3,10 @@ import os
 import google.generativeai as genai
 
 import streamlit as st
-from dotenv import load_dotenv
-load_dotenv()
+#from dotenv import load_dotenv
+#load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY") or st.secrets["GEMINI_API_KEY"]
-
+    
 genai.configure(api_key=api_key)
 
 
